8428982442a356d2f4c12cbdd3421333140c7c82
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-02 01:19:01 -0700

    made play.js accept incoming command line keystrokes like ctrl+c. I think I did the next step too by sending data to the server on which direction to move

--- play.js
+++ play.js
@@ -1,2 +1,20 @@
 const { connect } = require('./client');
-connect();
+const conn = connect();
+const handleUserInput = function (key) {
+  if (key === '\u0003') process.exit();
+  if (key === '\u001b[A') conn.write('Move: up');
+  if (key === '\u001b[B') conn.write('Move: down');
+  if (key === '\u001b[C') conn.write('Move: right');
+  if (key === '\u001b[D') conn.write('Move: left');
+
+};
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+setupInput();
