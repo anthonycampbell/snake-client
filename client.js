8b0ed84a86b1b913dbe4aee04e8f78b7dc0ebcbd
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-02 00:56:56 -0700

    refactored connection to its own file

---
+++ client.js
@@ -0,0 +1,15 @@
+const net = require("net");
+const connect = function() {
+  const conn = net.createConnection({
+    host: '192.168.1.77',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on('data', (msg) => console.log(msg));
+  console.log("Connecting ...");
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,17 +1,2 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '192.168.1.77',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on('data', (msg) => console.log(msg));
-  return conn;
-};
-
-console.log("Connecting ...");
+const { connect } = require('./client');
 connect();
